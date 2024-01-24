bdc9ab69485ea0c8ca309ea31b1bfcdfb1d8044a
Joao-Pedro-P-Holanda <110267046+Joao-Pedro-P-Holanda@users.noreply.github.com>
2024-01-24 13:38:20 +0000

    updating template with my informations

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "João Pedro Holanda",
+  title: "Software Engineering Student",
+  gitHub: "Joao-Pedro-P-Holanda",
+  linkedIn: "joão-pedro-holanda-755024272",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -23,29 +23,20 @@ const imageAltText = "purple and blue abstract background";
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+const description = `Software Engineering student at the Federal University of Ceará, currently focusing in back-end development. Some topics of my interest are:`;
 
 /**
  * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
+ * passionate about, or enjoy.
  */
-const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
-];
+const skillsList = ["Web development", "REST APIs", "Data Analysis", "Java", "Python", "SQL"];
 
 /**
  * Use this to give more information about what you are passionate about,
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
-const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+const detailOrQuote = "";
 
 const About = () => {
   return (
